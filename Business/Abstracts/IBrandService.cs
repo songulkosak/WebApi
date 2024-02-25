25bb57fa720d04da045a2c970822e24c4bf0e484
kosaksongul <kosaksongul@gmail.com>
2024-02-25 21:41:24 +0300

    Add project files.

---
+++ Business/Abstracts/IBrandService.cs
@@ -0,0 +1,20 @@
+﻿using Business.Dta.Request;
+using Business.Dta.Responses;
+using Entities.Concretes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstracts
+{
+    public interface IBrandService
+    {
+        CreatedBrandResponse Add(CreatedBrandRequest brand);
+        List<Brand> GetAll();
+
+    }
+
+    //responses and request
+}
